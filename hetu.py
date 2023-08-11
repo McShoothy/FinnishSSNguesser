1b421ae76cc706c462f436ace7a8d2af1840a158
Sam Z <46942016+McShoothy@users.noreply.github.com>
2023-08-11 14:34:09 +0300

    Update HETU.py
    Made it so that it takes arguments instead of boring imputs

--- hetu.py
+++ hetu.py
@@ -1,13 +1,20 @@
-list = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","H","J","K","l","M","N","P","R","S","T","U","V","W","X","Y"]
-bday = input("birthday (DD/MM/YY):")
-gender = raw_input("gender (M/F): ")
-ccs = raw_input("last charecter of your SSN:")
-CS = list.index(ccs)
-print("Possible SSNs:")
-#if the birthday is before the year 2000 just change the "A" to a "-" // im just way to lazy to add that simple function right now...or ever. 
+lst = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","H","J","K","L","M","N","P","R","S","T","U","V","W","X","Y"]
 
-for i in range(0,1000):
-      if (i+bday*1000)%31 == (CS) and gender == ("M") and i%2 == (1):
-        print bday,"A",i, ccs
-      elif (i+bday*1000)%31 == (CS) and gender == ("F") and i%2 == (0):
-        print bday,"A",i,ccs
+def generate_possible_ssns(bday, gender, ccs):
+    CS = lst.index(ccs)
+
+    print("Possible SSNs:")
+
+    for i in range(0, 1000):
+        if (i + int(bday) * 1000) % 31 == CS and (gender == "M" and i % 2 == 1) or (gender == "F" and i % 2 == 0):
+            print(f"{bday}A{i} {ccs}")
+
+def main():
+    bday = input("birthday (DD/MM/YY): ")
+    gender = input("gender (M/F): ")
+    ccs = input("last character of your SSN: ")
+
+    generate_possible_ssns(bday, gender, ccs)
+
+if __name__ == "__main__":
+    main()
